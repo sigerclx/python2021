f2324a37ee65eaae38524a0e589ae49f772a68fc
jacky <2000clx@126.com>
2021-12-01 20:09:54 +0800

    test

--- 202111/爬虫-小说/126shu.py
+++ 202111/爬虫-小说/126shu.py
@@ -5,9 +5,10 @@ import requests,time,sys
 from requests import RequestException
 # 注意 本程序的 request 和 urllib3 版本不能太高
 
-page = 454
+page = 92
 
 def downloadPage(url):
+    # 下载txt小说的接口
     txturl = 'https://down.bqg99.org/modules/article/txtarticle.php?id='
     webhtml = Downbook()
     #print(getPage.geturlcontent(url))
@@ -23,10 +24,11 @@ def downloadPage(url):
 
     #print(len(htmlclass))
 
-
+    #找到下载链接起始的特征
     xpath ='//div[@class="l"]//span[@class="s2"]/a'
     #xpath ='//div[@style="min-width: 600px"]/a'
 
+    # 获取链接的class
     hclass = htmlclass.xpath(xpath)
     #restxt = webhtml.xpathdecode(hclass)
     print('-'*30,len(hclass),'-'*30)
@@ -57,9 +59,9 @@ def downloadPage(url):
 
 
 
-for i in range(220,page):
+for i in range(1,page):
 
-    url = "https://www.126shu.org/novel-list-1-" +str(i)+".html"
+    url = "https://www.126shu.org/novel-list-2-" +str(i)+".html"
     print('准备下载 , 第 ',i,' 页',url)
     downloadPage(url)
 
