5d7e35d897edbf56bc6324091417df4302609e76
jacky <2000clx@126.com>
2022-05-11 00:08:08 +0800

    flask 蓝图main

--- 2022/flaskOne/app/routes.py
+++ 2022/flaskOne/app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from datetime import datetime
 import math
 from app import app,baseDict
-#from app.forms import LoginForm
+from app.forms import EmptyForm
 from flask_login import login_required
 from app.models import User,Reimbursement
 
@@ -36,39 +36,8 @@ from flask import request
 
 
 from app import db
-# from app.forms import RegistrationForm,EditProfileForm, EmptyForm
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user is None or not user.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-#         login_user(user, remember=form.remember_me.data)
-#         next_page = request.args.get('next')
-#         if not next_page or url_parse(next_page).netloc != '':
-#             next_page = url_for('index')
-#         return redirect(next_page)
-#
-#     return render_template('login.html', title='Sign In', form=form)
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, email=form.email.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('login'))
-#     return render_template('register.html', title='Register', form=form)
+
 
 @app.before_request
 def before_request():
@@ -107,21 +76,6 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile',form=form)
 
 
-# from app.forms import ReimbursementForm
-#
-# @login_required
-# @app.route('/reimbursement', methods=['GET', 'POST'])
-# def reimbursement():
-#     form = ReimbursementForm()
-#     if form.validate_on_submit():
-#         record = Reimbursement(source=form.source.data, name=form.name.data,qty=float(form.qty.data),total=float(form.total.data))
-#         db.session.add(record)
-#         db.session.commit()
-#         #aaaa.append(form.source.data)
-#         return redirect(url_for('index'))
-#         #flash('Congratulations, you are now a registered user!')
-#         #return redirect(url_for('login'))
-#     return render_template('reimbursement.html', title='采购记录', form=form)
 
 
 @app.route('/follow/<username>', methods=['POST'])
